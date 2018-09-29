000333fb614d87eccdd5b15efa0e407ac9d6cd67
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2018-09-29 09:07:49 +0200

    cleaned up the code a bit

--- app.R
+++ app.R
@@ -71,11 +71,28 @@ data_input_ui <- function() {
 analysis_ui <- function(x) {
   sidebarLayout(
     sidebarPanel(
-      numericInput("x_axis_dimension","Dimension x-axis", value = 1, min=1),
-      numericInput("y_axis_dimension","Dimension y-axis", value = 2, min=1),
-      checkboxGroupInput("display_what", "What to display", choices = list("Sites"="sites", "Types"="species"), selected = c("sites", "species")),
-      checkboxGroupInput("display_how", "Plot Elements", choices = list("Points"="points", "Text"="text"), selected = c("points", "text")),
-      checkboxInput("spead_labels", "Spread labels")
+      numericInput("x_axis_dimension",
+                   "Dimension x-axis",
+                   value = 1,
+                   min=1),
+      numericInput("y_axis_dimension",
+                   "Dimension y-axis",
+                   value = 2,
+                   min=1),
+      checkboxGroupInput("display_what",
+                         "What to display",
+                         choices = list("Sites"="sites",
+                                        "Types"="species"),
+                         selected = c("sites",
+                                      "species")),
+      checkboxGroupInput("display_how",
+                         "Plot Elements",
+                         choices = list("Points"="points",
+                                        "Text"="text"),
+                         selected = c("points",
+                                      "text")),
+      checkboxInput("spead_labels",
+                    "Spread labels")
     ),
     mainPanel(
       
@@ -83,11 +100,15 @@ analysis_ui <- function(x) {
       plotOutput("plot"),
       
       # Input: Choose dataset ----
-      selectInput("download_format", "Download as:",
-                  choices = c("svg", "png", "pdf")),
+      selectInput("download_format",
+                  "Download as:",
+                  choices = c("svg",
+                              "png",
+                              "pdf")),
       
       # Button
-      downloadButton("downloadPlot", "Download")
+      downloadButton("downloadPlot",
+                     "Download")
     )
   )
 }
@@ -95,26 +116,38 @@ analysis_ui <- function(x) {
 download_ui <- function() {
   mainPanel(
     
-    downloadButton("downloadResult", "Download Coordinates of Points")
-    
+    downloadButton("downloadResult",
+                   "Download Coordinates of Points")
     
   )
 }
 
-ui <- shinyUI(navbarPage(id="navbar", title = "Tool for Online Seriation and Correspondence Analysis (TOSCA)",
-                         tabPanel(id="data_input",title = "Data Input", data_input_ui()),
-                         tabPanel("Analysis", id="analysis", analysis_ui()),
-                         tabPanel("Export", download_ui())
+ui <- shinyUI(navbarPage(id="navbar",
+                         title = "Tool for Online
+                         Seriation and Correspondence Analysis (TOSCA)",
+                         tabPanel(id="data_input",
+                                  title = "Data Input",
+                                  data_input_ui()),
+                         tabPanel("Analysis",
+                                  id="analysis",
+                                  analysis_ui()),
+                         tabPanel("Export",
+                                  download_ui())
 ))
 
 # Define server logic to read selected file ----
 server <- function(input, output, session) {
   observeEvent(input$go, {
-    updateTabsetPanel(session = session, inputId = "navbar", selected = "Analysis")
+    updateTabsetPanel(session = session,
+                      inputId = "navbar",
+                      selected = "Analysis")
   })
   observe({
-    updateCheckboxInput(session, "spead_labels", value = F)
-    shinyjs::toggleState("spead_labels", 'text' %in% input$display_how)
+    updateCheckboxInput(session,
+                        "spead_labels",
+                        value = F)
+    shinyjs::toggleState("spead_labels",
+                         'text' %in% input$display_how)
   })
   output$contents <- renderTable({
     
@@ -149,26 +182,53 @@ server <- function(input, output, session) {
   })
   
   plot_ca <- function(ca) {
-    test_for_ggplot <- melt(scores(ca, choices = c(input$x_axis_dimension,input$y_axis_dimension), display = input$display_what, scaling = 3))
+    test_for_ggplot <- melt(
+      scores(ca,
+             choices = c(input$x_axis_dimension,
+                         input$y_axis_dimension),
+             display = input$display_what,
+             scaling = 3)
+    )
     
-    test_for_ggplot<-spread(test_for_ggplot, key=Var2, value = value)
+    test_for_ggplot<-spread(test_for_ggplot,
+                            key=Var2,
+                            value = value)
     
     test_for_ggplot <- data.frame(name = test_for_ggplot$Var1,
                                   x = test_for_ggplot[,ncol(test_for_ggplot)-1],
                                   y=test_for_ggplot[,ncol(test_for_ggplot)])
     
-    test_for_ggplot$type <- if("L1" %in% names(test_for_ggplot)) test_for_ggplot$L1 else input$display_what
+    test_for_ggplot$type <- if("L1" %in% names(test_for_ggplot))
+    {
+      test_for_ggplot$L1
+    }
+    else
+    {
+      input$display_what
+    }
     
-    my_plot <- ggplot(test_for_ggplot)# + geom_point(aes(x=x,y=y,col=type)) + geom_text_repel(aes(x=x,y=y,label=name),size=2)
+    my_plot <- ggplot(test_for_ggplot)
     
     if("points" %in% input$display_how) {
-      my_plot <- my_plot + geom_point(aes(x=x,y=y,col=type))
+      my_plot <- my_plot + geom_point(
+        aes(x = x,
+            y = y,
+            col = type)
+      )
     }
     if("text" %in% input$display_how) {
       if(input$spead_labels) {
-        my_plot <- my_plot + geom_text_repel(aes(x=x,y=y,label=name),size=2)
+        my_plot <- my_plot + geom_text_repel(
+          aes(x = x,
+              y = y,
+              label = name),
+          size=2)
       } else {
-        my_plot <- my_plot + geom_text(aes(x=x,y=y,label=name))
+        my_plot <- my_plot + geom_text(
+          aes( x = x,
+               y = y,
+               label = name)
+          )
       }
     }
     
@@ -181,9 +241,15 @@ server <- function(input, output, session) {
   })
   
   output$downloadPlot <- downloadHandler(
-    filename = function() { paste(input$file1$name, '.', input$download_format, sep='') },
+    filename = function() {
+      paste(input$file1$name,
+            '.',
+            input$download_format, sep='')
+      },
     content = function(file) {
-      ggsave(file, plot = plot_ca(ca()), device = input$download_format)
+      ggsave(file,
+             plot = plot_ca(ca()),
+             device = input$download_format)
     }
   )
   
@@ -192,14 +258,24 @@ server <- function(input, output, session) {
       paste(input$dataset, ".csv", sep = "")
     },
     content = function(file) {
-      test_for_export <- melt(scores(ca(), choices = c(input$x_axis_dimension,input$y_axis_dimension), display = input$display_what, scaling = 3))
-      test_for_export <- spread(test_for_export, key=Var2, value = value)
+      test_for_export <- melt(
+        scores(ca(),
+               choices = c(input$x_axis_dimension,
+                           input$y_axis_dimension),
+               display = input$display_what,
+               scaling = 3)
+        )
+      test_for_export <- spread(test_for_export,
+                                key=Var2,
+                                value = value)
       
       test_for_export <- data.frame(name = test_for_export$Var1,
                                     x = test_for_export[,ncol(test_for_export)-1],
                                     y=test_for_export[,ncol(test_for_export)])
-
-      write.csv(test_for_export, file, row.names = FALSE)
+      
+      write.csv(test_for_export,
+                file,
+                row.names = FALSE)
     }
   )
 }
